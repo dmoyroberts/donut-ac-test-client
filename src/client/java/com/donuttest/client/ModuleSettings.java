6c11a6f7081e3e57bd0d854b3a888246c4f1098a
dmoyroberts <roberrsdmoy@gmail.com>
2026-09-03 17:45:01 -0400

    Add src/client/java/com/donuttest/client/ModuleSettings.java

---
+++ src/client/java/com/donuttest/client/ModuleSettings.java
@@ -0,0 +1,14 @@
+package com.donuttest.client;
+
+public final class ModuleSettings {
+    public boolean playerTracers = true;
+    public boolean spawnerEsp = true;
+    public boolean storageEsp = true;
+    public boolean redstoneEsp = true;
+    public boolean stashFinder = true;
+    public boolean baseFinder = true;
+    public boolean chunkOverlay = true;
+    public boolean privacyAudit = true;
+    public int scanRadius = 48;
+    public int stashThreshold = 6;
+}
